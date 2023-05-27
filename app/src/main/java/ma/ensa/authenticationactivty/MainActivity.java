6ce88e4c2aa1a8d77c53055e462e248ede5c40c1
Nour <cherrat.nour@gmail.com>
2023-05-27 11:25:49 +0100

    Initial commit

---
+++ app/src/main/java/ma/ensa/authenticationactivty/MainActivity.java
@@ -0,0 +1,33 @@
+package ma.ensa.authenticationactivty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button btnLogin;
+        btnLogin = findViewById(R.id.login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),TransactionActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+}
